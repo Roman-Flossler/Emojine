3e3e2b57421ea5e5e294bc0e9f6660394392bcc9
Roman Flössler <flor@flor.cz>
2020-08-19 22:00:36 +0200

    Initial commit

---
+++ precache-manifest.38773ab9875a320309cad66875d1a524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d8546ff3e7501aca27b90eb4f73049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f7a4bfd0131c7f3adb",
+    "url": "/static/css/main.0c2820ee.chunk.css"
+  },
+  {
+    "revision": "ffed7584c0514e267176",
+    "url": "/static/js/2.86907592.chunk.js"
+  },
+  {
+    "revision": "00f7a4bfd0131c7f3adb",
+    "url": "/static/js/main.fe4993c5.chunk.js"
+  },
+  {
+    "revision": "b2f1485d78f67867afa9",
+    "url": "/static/js/runtime~main.effc2e87.js"
+  }
+]);
